3ad64994cf04f08293cc574e5320e2224de1549e
Cejas Walter Nicolas <nicolascejas511@gmail.com>
2023-09-11 21:56:09 -0300

    PreEntrega1Cejas

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import CartWidget from './components/CartWidget/CartWidget'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+  
+
+  return (
+    
+      <NavBar />
+      
+      
+  )
+}
+
+export default App
---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CartWidget = () => {
+    return (
+        <div>
+            <i className="bi bi-bag"></i>
+        </div>
+    )
+}
+export default CartWidget
